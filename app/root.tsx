4e9c51cf590f816815551d906fa9194a0d087025
Saar Davidson <davidsonsaar@gmail.com>
2023-09-12 17:46:23 +0300

    add auth

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+
+import type { V2_MetaFunction, LoaderFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,17 +9,45 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+ 
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+
+import { ClerkApp, V2_ClerkErrorBoundary } from "@clerk/remix";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
+ 
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Enpilist | Remix" },
+    {
+      property: "og:title",
+      content: "Enpilist",
+    },
+    {
+      name: "description",
+      content: "This app is the best",
+    },
+    {
+      property: "charset",
+      content: "utf-8",
+    },
+    {
+      property: "viewport",
+      content: "width=device-width,initial-scale=1",
+    }
+  ];
+};
+ 
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+ 
+export const ErrorBoundary = V2_ClerkErrorBoundary();
 
-export default function App() {
+function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
@@ -31,3 +60,5 @@ export default function App() {
     </html>
   );
 }
+
+export default ClerkApp(App);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,12 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
+import {
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  UserButton,
+} from "@clerk/remix";
+
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -7,35 +14,18 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+ 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Enpilist</h1>
+      <SignedIn>
+        <p>You are signed in!</p>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </div>
   );
 }
